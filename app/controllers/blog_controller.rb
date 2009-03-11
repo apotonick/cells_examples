3487c63e55ecb1798103ccf2bf5c36e675ca50d3
Nick Sutterer <apotonick@gmail.com>
2009-03-11 23:27:02 +0100

    cells examples now running in a rails 2.3.1 environment.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,4 +1,5 @@
-class BlogController < ApplicationController
+class BlogController < ActionController::Base
+
   def index
   end
 
--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
+#require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
 require File.join(File.dirname(__FILE__), '../vendor/plugins/cells/boot')
 
 Rails::Initializer.run do |config|
