01783962c738b2135a688834b3942c5f72322718
Nick Sutterer <apotonick@gmail.com>
2009-04-02 22:15:25 +0200

    added an example state in PostCell which uses state caching and expiring via a Sweeper. added sqlite example database, so you can try-out easier. michal, thanks for inspiration.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,8 +1,15 @@
 class BlogController < ActionController::Base
-
+  cache_sweeper :posts_sweeper
+  
   def index
   end
-
+  
+  
+  def add
+    Post.new(:title => "yo!", :body=>Time.now.to_s).save
+    render :text => "added a posting"
+  end
+  
   def dashboard
   end
 
