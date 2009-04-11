0ea67d0af8c881be29c2771916a626706344a780
mlomnicki <michal.lomnicki@gmail.com>
2009-04-12 01:21:22 +0200

    blog/index is root now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.root :controller => 'blog'
   map.resources :videos
   
   # The priority is based upon order of creation: first created -> highest priority.
